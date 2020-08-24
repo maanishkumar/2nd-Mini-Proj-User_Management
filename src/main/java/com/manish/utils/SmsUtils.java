99b176e71e7e5689efcc45f54105a6def0c69066
maanishkumar <65637365+maanishkumar@users.noreply.github.com>
2020-08-23 22:19:07 -0700

    Update SmsUtils.java

--- src/main/java/com/manish/utils/SmsUtils.java
+++ src/main/java/com/manish/utils/SmsUtils.java
@@ -8,15 +8,15 @@ import com.twilio.type.PhoneNumber;
 
 @Component
 public class SmsUtils {
-	private final static String ACCOUNT_SID = "AC96db782debe8ff08a7487a1988ab029e";
-	private final static String AUTH_ID = "a7236a888299e48c99a410148a82fa40";
+	private final static String ACCOUNT_SID = "...";
+	private final static String AUTH_ID = "......";
 	static {
 		Twilio.init(ACCOUNT_SID, AUTH_ID);
 	}
 
 	public void sendUserSmsOtp(String password) {
 		 Message.creator(new PhoneNumber("+919931765210"),
-					             new PhoneNumber("+15046080926"),
+					             new PhoneNumber("+1504608"),
 					             "Your user app login pwd sent by manish="+password)
 				                 .create();
 		
